e46b3a20c66240808f492640546245cdfcd6d1fd
gagdiez <gagdiez.c@gmail.com>
2021-02-18 11:19:15 +0100

    updated interface - more interactive

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,8 +2,9 @@ import {initNEAR, login, logout, get_pool_info, get_account,
         stake, unstake, withdraw, raffle, update_prize,
         get_winners} from './blockchain'
 
-async function display_pool_info(){
-  let pool = await get_pool_info()
+async function get_and_display_pool_info(){
+  // reset ui
+  window.pool = await get_pool_info()
 
   $('.pool-tickets').html(pool.total_staked.toFixed(2))
   $('.pool-prize').html(pool.prize.toFixed(2))
@@ -17,20 +18,42 @@ async function display_pool_info(){
 
   let winners = await get_winners()
 
+  $('#winners').html('')
   for (var i = 0; i < winners.length; i++) {
     $('#winners').append(`<li>${winners[i]}</li>`);
   }
 }
 
 async function login_flow(){
-
-  await update_prize()
-
   $('#account').html(window.walletAccount.accountId+' <i class="fas fa-caret-down"></i>')
 
-  let user = await get_account(window.walletAccount.accountId)
+  if(pool.next_prize_tmstmp < Date.now()){
+    await raffle()
+  }else{
+    await update_prize()
+  }
+
+  get_and_display_user_info()
+  get_and_display_pool_info()
+}
+
+async function get_and_display_user_info(){
+  // reset ui
+  const spin = '<span class="fas fa-sync fa-spin"></span>'
+  $('.user-staked').html(spin)
+  $('.user-unstaked').html(spin)
+  $('#user-odds').html(spin)
+  $('#btn-leave').hide()
+  $('#withdraw_btn').hide()
+  $('#withdraw-msg').show()
+  $('#withdraw-countdown').hide()
+
+  window.user = await get_account(window.walletAccount.accountId)
   $('.user-staked').html(user.staked_balance.toFixed(2))
   $('.user-unstaked').html(user.unstaked_balance.toFixed(2))
+  $('#user-odds').html((user.staked_balance/pool.total_staked).toFixed(2))
+
+  if(user.staked_balance > 0){$('#btn-leave').show()}
 
   if(user.unstaked_balance > 0 && !user.available){
     $('#withdraw_btn').hide()
@@ -48,19 +71,11 @@ async function login_flow(){
     $('#withdraw-msg').html("You can withdraw your NEAR!");
     $('#withdraw_btn').show()
   }
-
-  window.user = user;
-  // we need the user to call "raffle" when the pool is ready to raffle
-
-  let pool = await get_pool_info()
-  $('#user-odds').html((user.staked_balance/pool.total_staked).toFixed(2))
-  if(pool.next_prize_tmstmp < Date.now()){raffle()}
 }
 
 
-function flow(){
-
-  display_pool_info()
+async function flow(){
+  await get_and_display_pool_info()
 
   if (!window.walletAccount.accountId){
     $(".logged-in").hide()
@@ -73,17 +88,16 @@ function flow(){
 window.login = login
 window.logout = logout
 window.withdraw = async function(){
-    $('.user-unstaked').html('<span class="fas fa-sync fa-spin"></span>')
     const results = await withdraw()
     if(result){
-      $('.user-unstaked').html('0')
+      get_and_display_user_info() 
     }else{
       $('.user-unstaked').html('try again later')
     }
 }
 
 window.buy_tickets = function(){
-  const toStake = parseInt($("#how-much-input").val());
+  const toStake = parseFloat($("#how-much-input").val()).toFixed(2);
   if (!isNaN(toStake)){
     stake(toStake);
   }
@@ -96,21 +110,12 @@ window.leave_pool = async function(){
     const result = await unstake(window.user.staked_balance);
 
     if(result){
-      $('.user-staked').html('0')
-      const total = user.staked_balance + user.unstaked_balance
-      $('.user-unstaked').html(total.toFixed(2))
+      get_and_display_user_info()
     }else{
       $('.user-staked').html('try later')
     }
   }
 }
-
-
-// NOTE: RETURNS TRUE IF IT SUCCEDED.. USE IT TO UPDATE THE INTERFACE
-window.update_prize = update_prize
-
-// NOTE: RETURNS the IDX of who won.. we could use it to keep track on a server... eventually
-window.raffle = raffle
 
 window.onload = function(){
   window.nearInitPromise = initNEAR()
