973ea02fe351e61a69ff9870bb4399d792a065d7
gagdiez <gagdiez.c@gmail.com>
2021-03-02 22:40:39 +0100

    reflected changes on website

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,13 +1,13 @@
 import {initNEAR, login, logout, get_pool_info, get_account,
         stake, unstake, withdraw, raffle, update_prize,
-        get_winners} from './blockchain'
+        get_winners, floor, unstake_external, withdraw_external} from './blockchain'
 
 async function get_and_display_pool_info(){
   // reset ui
   window.pool = await get_pool_info()
-
-  $('.pool-tickets').html(pool.total_staked.toFixed(2))
-  $('.pool-prize').html(pool.prize.toFixed(2))
+  console.log(pool)
+  $('.pool-tickets').html(pool.total_staked)
+  $('.pool-prize').html(pool.prize)
 
   $("#time-left")
   .countdown(pool.next_prize_tmstmp, function(event) {
@@ -20,17 +20,22 @@ async function get_and_display_pool_info(){
 
   $('#winners').html('')
   for (var i = 0; i < winners.length; i++) {
-    $('#winners').append(`<li>${winners[i]}</li>`);
+    $('#winners').append(`<li>${winners[i].account_id} - ${winners[i].amount} N</li>`);
   }
 }
 
 async function login_flow(){
   $('#account').html(window.walletAccount.accountId+' <i class="fas fa-caret-down"></i>')
 
-  if(pool.next_prize_tmstmp < Date.now()){
+  if(pool.next_prize_tmstmp < Date.now() && pool.total_staked>0){
     await raffle()
   }else{
     await update_prize()
+  }
+
+  if(pool.withdraw_ready){
+    await withdraw_external()
+    await unstake_external()
   }
 
   get_and_display_user_info()
@@ -49,9 +54,14 @@ async function get_and_display_user_info(){
   $('#withdraw-countdown').hide()
 
   window.user = await get_account(window.walletAccount.accountId)
-  $('.user-staked').html(user.staked_balance.toFixed(2))
-  $('.user-unstaked').html(user.unstaked_balance.toFixed(2))
-  $('#user-odds').html((user.staked_balance/pool.total_staked).toFixed(2))
+  $('.user-staked').html(user.staked_balance)
+  $('.user-unstaked').html(user.unstaked_balance)
+  
+  if(user.staked_balance > 0){
+    $('#user-odds').html((user.staked_balance/pool.total_staked).toFixed(2))
+  }else{
+    $('#user-odds').html(0)
+  }
 
   if(user.staked_balance > 0){$('#btn-leave').show()}
 
@@ -59,12 +69,8 @@ async function get_and_display_user_info(){
     $('#withdraw_btn').hide()
     $('#withdraw-msg').hide()
     $('#withdraw-countdown').show()
-    $("#withdraw-time-left")
-    .countdown(user.available_when, function(event) {
-      $(this).text(
-        event.strftime('%d days %H:%M:%S')
-      );
-     });
+
+    $("#withdraw-time-left").html(user.available_when)
   }
 
   if(user.available){
@@ -72,7 +78,6 @@ async function get_and_display_user_info(){
     $('#withdraw_btn').show()
   }
 }
-
 
 async function flow(){
   await get_and_display_pool_info()
@@ -85,25 +90,14 @@ async function flow(){
   }
 }
 
-window.login = login
-window.logout = logout
-window.withdraw = async function(){
-    const results = await withdraw()
-    if(result){
-      get_and_display_user_info() 
-    }else{
-      $('.user-unstaked').html('try again later')
-    }
-}
-
 window.buy_tickets = function(){
-  const toStake = parseFloat($("#how-much-input").val()).toFixed(2);
+  $('#buy-btn').html('<span class="fas fa-sync fa-spin text-white"></span>')
+  const toStake = floor($("#how-much-input").val());
   if (!isNaN(toStake)){
     stake(toStake);
   }
 }
 
-// NOTE: RETURNS TRUE IF IT SUCCEDED.. USE IT TO UPDATE THE INTERFACE
 window.leave_pool = async function(){
   if(window.user.staked_balance > 0){
     $('.user-staked').html('<span class="fas fa-sync fa-spin"></span>')
@@ -111,10 +105,24 @@ window.leave_pool = async function(){
 
     if(result){
       get_and_display_user_info()
-    }else{
-      $('.user-staked').html('try later')
+      get_and_display_pool_info()
     }
   }
+}
+
+window.unstake = unstake
+window.unstake_external = unstake_external
+window.withdraw_external = withdraw_external
+
+window.withdraw = async function(){
+    $('.user-unstaked').html('<span class="fas fa-sync fa-spin"></span>')
+    const result = await withdraw()
+
+    if(result){
+      get_and_display_user_info() 
+    }else{
+      $('.user-unstaked').html('try again later')
+    }
 }
 
 window.onload = function(){
@@ -122,3 +130,7 @@ window.onload = function(){
   .then(flow)
   .catch(console.error)
 }
+
+window.login = login
+window.logout = logout
+
