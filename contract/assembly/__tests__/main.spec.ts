5477eb4f417c8389aa910d2a01d06d48d0a30a45
gagdiez <gagdiez.c@gmail.com>
2021-04-06 12:26:33 +0200

    More tests

--- contract/assembly/__tests__/main.spec.ts
+++ contract/assembly/__tests__/main.spec.ts
@@ -18,9 +18,7 @@ describe("Random", () => {
       expect(rnd >= u128.Zero && u128.from(max) > rnd)
 
       for(let j=0; j < max; j++){
-        if(rnd >= u128.from(j) && u128.from(j+1) > rnd){
-          numbers[j] = numbers[j] + 1
-        }
+        if(rnd == u128.from(j)){ numbers[j] = numbers[j] + 1 }
       }
     }
 
@@ -29,6 +27,37 @@ describe("Random", () => {
     }
   });
 });
+
+describe("Random", () => {
+  it("should be random from a min to a max", () => {
+    let trials = 100
+    let min = 3
+    let max = 13
+    let total = 15
+    let numbers = new Array<i32>()
+    
+    for(let i=0; i < total; i++){numbers.push(0)}
+
+    for(let i=0; i < trials; i++){
+      let rnd:u128 = random_u128(u128.from(min), u128.from(max))
+
+      expect(rnd >= u128.Zero && u128.from(max) > rnd)
+
+      for(let j=0; j < total; j++){
+        if(rnd == u128.from(j)){ numbers[j] = numbers[j] + 1 }
+      }
+    }
+    for(let i=0; i < min; i++){ expect(numbers[i] == 0 ).toBe(true) }
+
+    for(let i=min; i < max; i++){
+      expect(numbers[i] > 5 && numbers[i] < 15).toBe(true)
+    }
+
+    for(let i=max; i < total; i++){ expect(numbers[i] == 0 ).toBe(true) }
+  });
+});
+
+
 
 describe("Binary Tree", () => {
   it("correctly stores/selects users", () => {
